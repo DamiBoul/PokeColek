6727ac484be74d6b9cc20c943c87fe28a306ea52
DamiBoul <damien.boulanger2@gmail.com>
2024-02-21 10:23:40 +0100

    arborescence

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LigneDeReponseModule } from './components/ligne-de-reponse/ligne-de-reponse.module';
-import { BadgeComponent } from '../badge/badge.component';
+import { BadgeComponent } from './components/badge/badge.component';
 
 @Component({
   selector: 'app-root',
