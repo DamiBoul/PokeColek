02d19f756b5560909c5a202e74990977a944b369
Barbier Lucie <s190655@ic2s110-16.univ-lemans.fr>
2024-02-21 11:15:59 +0100

    J'ai pleuré sur le background :'(

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,15 +2,20 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LigneDeReponseModule } from './components/ligne-de-reponse/ligne-de-reponse.module';
 import { BadgeComponent } from '../badge/badge.component';
+import { NgStyle } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, LigneDeReponseModule, BadgeComponent],
+  imports: [RouterOutlet, LigneDeReponseModule, BadgeComponent, NgStyle],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
+ // bgColor;
+  constructor(){
+   // this.bgColor = '191919' ;
+  }
   title = 'Napoleon';
   spectre="";
   type = ["acier", "combat", "dragon", "eau", "electrik", "fee", "feu", "glace", "insecte", "normal", "plante", "poison", "psy", "roche", "sol", "spectre", "tenebres", "vol"];
