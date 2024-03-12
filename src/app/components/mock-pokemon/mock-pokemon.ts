ca7c92d720cae5468719c5d91fbd139428c77d46
Barbier Lucie <s190655@ic2s112-15.univ-lemans.fr>
2024-03-12 10:43:17 +0100

    affichage dynamique caractéristiques pokémon

---
+++ src/app/components/mock-pokemon/mock-pokemon.ts
@@ -0,0 +1,16 @@
+import { Pokemon } from "../pokemon/pokemon.component";
+
+export const POKEMONS: Pokemon[] = [
+    {
+        id: 1,
+        name: "Bulbizarre",
+        image: "https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png",
+        type1: "plante",
+        type2: "poison",
+        gen: 1,
+        stade: 1,
+        taille: 0.5,
+        poids: 12,
+        talent: ["overgrowth", "chlorophyll"]
+    }
+];
