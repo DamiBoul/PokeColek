c3555ac11c6910d059423b2be5abcd0696013f82
DamiBoul <damien.boulanger2@gmail.com>
2024-03-16 20:13:54 +0100

    squelette collection

--- src/app/components/element-collek/element-collek.component.ts
+++ src/app/components/element-collek/element-collek.component.ts
@@ -7,6 +7,13 @@ import { Component } from '@angular/core';
   templateUrl: './element-collek.component.html',
   styleUrl: './element-collek.component.css'
 })
-export class ElementCollekComponent {
-
+export class ElementCollek {
+  id!: Number; //numéro d'identification du collectionnable
+  cat!: Number; /**indique à quelle catégorie l'objet appartient (
+                                                                  1 - Pokémon
+                                                                  2 - Objet
+                                                                  3 - Capacité
+                                                                  4 - Lieu
+                                                                  ) 
+    */
 }
