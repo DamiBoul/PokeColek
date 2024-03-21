87724ec1e03c2da3f58b269af9a4c49e3d032531
HiraAkshayy <henzoageorges@gmail.com>
2024-03-21 13:47:19 +0100

    Suppression des traces de BDD

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { SettingService } from './services/settings-service.service';
 import { Router } from '@angular/router';
-import firebase from 'firebase/app';
 
 @Component({
   selector: 'app-root',
@@ -24,16 +23,7 @@ export class AppComponent implements OnInit{
   }
 
   ngOnInit(){
-    /*const firebaseConfig = {
-      apiKey: "AIzaSyDMQ9cDfkAtWnGz42KTE8R1ljkNR-w06Us",
-      authDomain: "pocollection-c15f2.firebaseapp.com",
-      projectId: "pocollection-c15f2",
-      storageBucket: "pocollection-c15f2.appspot.com",
-      messagingSenderId: "493665883497",
-      appId: "1:493665883497:web:73b691664028a03b6399f8",
-      measurementId: "G-9FYZLELYZ0"
-    };
-    firebase.initializeApp(firebaseConfig);*/
+    
   }
 
   navigateTo(route: string) {
